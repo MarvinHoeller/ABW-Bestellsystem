88361029ac64fa2414606265773c89ddce7520f5
Riko Gerdes <riko.gerdes@gmail.com>
2023-04-10 12:22:25 +0200

    fixed mongo-init.sh

---
+++ mongo-init.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+echo 'Start #################################################################'
+
+set -e
+
+mongo <<EOF
+use admin
+db = db.getSiblingDB($MONGO_INITDB_DATABASE);
+db.createUser(
+  {
+    user: "$MONGO_INITDB_ROOT_USERNAME",
+    pwd: "$MONGO_INITDB_ROOT_PASSWORD",
+    roles: [{ role: 'readWrite', db: "$MONGO_INITDB_DATABASE" }],
+  },
+);
+EOF
+
+echo 'END #################################################################'
