2e8cc723f7d38c1e30457801778e6f838f37b0c2
Riko Gerdes <riko.gerdes@gmail.com>
2023-04-22 16:35:19 +0200

    🐞 fix: empty env

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,10 +4,10 @@ import react from '@vitejs/plugin-react-swc'
 
 /** @type {import('vite').UserConfig} */
 export default ({ mode }: { mode: string }) => {
-  
+
   if (process.env.VITE_SUBFOLDER && !process.env.VITE_SUBFOLDER.startsWith('/'))
     process.env.VITE_SUBFOLDER = '/' + process.env.VITE_SUBFOLDER;
-
+  
   return defineConfig({
     plugins: [react(), tsconfigPaths()],
     base: mode === 'development' ? '/' : process.env.VITE_SUBFOLDER ?? '/'
