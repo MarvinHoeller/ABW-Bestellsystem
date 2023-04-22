a381096955a01550e0c67fd92a2c01011976e23a
Riko Gerdes <riko.gerdes@gmail.com>
2023-04-22 16:47:52 +0200

    🐞 fix: added slash at the end

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -7,7 +7,10 @@ export default ({ mode }: { mode: string }) => {
 
   if (process.env.VITE_SUBFOLDER && !process.env.VITE_SUBFOLDER.startsWith('/'))
     process.env.VITE_SUBFOLDER = '/' + process.env.VITE_SUBFOLDER;
-  
+
+  if (process.env.VITE_SUBFOLDER && !process.env.VITE_SUBFOLDER.endsWith('/'))
+    process.env.VITE_SUBFOLDER = process.env.VITE_SUBFOLDER + '/';
+
   return defineConfig({
     plugins: [react(), tsconfigPaths()],
     base: mode === 'development' ? '/' : process.env.VITE_SUBFOLDER ?? '/'
