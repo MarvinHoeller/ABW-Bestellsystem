77e8407e46489887f56324a32fa33fd82beddd72
Riko Gerdes <riko.gerdes@gmail.com>
2023-05-01 21:02:17 +0200

    reworked popup

--- frontend/src/Pages/dashboardPages/modules/OrderPopup.tsx
+++ frontend/src/Pages/dashboardPages/modules/OrderPopup.tsx
@@ -25,24 +25,24 @@ export function OrderPopup({
   if (!menuAdditions) return <></>;
 
 
-  const [productCount, setProductCount] = useState<number>(popUpData.quantity);
+  const [productCount, setProductCount] = useState<number>(1);
   const [sauces, setSauce] = useState<IsauceSchema>({
-    ketchup: popUpData.sauce?.ketchup ?? 0,
-    mustard: popUpData.sauce?.mustard ?? 0,
-    sweetMustard: popUpData.sauce?.sweetMustard ?? 0,
+    ketchup: 0,
+    mustard: 0,
+    sweetMustard: 0,
   });
-  const [comments, setComments] = useState<string[]>(popUpData.comment ?? []);
+  const [comments, setComments] = useState<string[]>([]);
 
   useEffect(() => {
     if (!modalShow) return;
-    setProductCount(popUpData.quantity);
+    setProductCount(1);
     setSauce({
-      ketchup: popUpData.sauce?.ketchup ?? 0,
-      mustard: popUpData.sauce?.mustard ?? 0,
-      sweetMustard: popUpData.sauce?.sweetMustard ?? 0,
+      ketchup: 0,
+      mustard: 0,
+      sweetMustard: 0,
     });
 
-    setComments(popUpData.comment ?? []);
+    setComments([]);
   }, [modalShow]);
 
   /**
@@ -75,23 +75,6 @@ export function OrderPopup({
     )} = ${formatToCurrent(calculated)}`;
   };
 
-  /**
-   * If the event target's name is comment, then set the comments state to the previous comments state
-   * plus the event target's value if the event target is checked, otherwise set the comments state to
-   * the previous comments state minus the event target's value.
-   *
-   * If the event target's name is not comment, then set the bread count state to the event target's
-   * value.
-   * @param event - React.ChangeEvent<HTMLInputElement>
-   */
-  // const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-
-  //   switch (event.target.name) {
-  //     case 'comment':
-
-  //       break;
-  //   }
-  // };
 
   return (
     //TODO: change some variables
@@ -111,199 +94,150 @@ export function OrderPopup({
           </Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          {isBreadSite ? (
-            <>
-              <Row>
-                <Col>
-                  <Form.Label>Brottyp</Form.Label>
-                  <Form.Select name="bread">
-                    <option value="normal">Normal</option>
-                    <option value="multigrain">Mehrkorn</option>
-                  </Form.Select>
-                </Col>
-              </Row>
-              <Row className="mt-3">
-                <Col>
-                  <Form.Label>Ketchup</Form.Label>
-                  <FormControl
-                    type="number"
-                    placeholder="Ketchup"
-                    name="ketchup"
-                    max={getSauceSum(sauces.ketchup)}
-                    min={0}
-                    value={sauces.ketchup}
-                    onChange={(event) => {
-                      setSauce((prev) => {
-                        return {
-                          ...prev,
-                          [event.target.name]: Number(event.target.value),
-                        };
-                      });
-                    }}
-                  />
-                </Col>
-                <Col>
-                  <Form.Label>Senf</Form.Label>
-                  <FormControl
-                    type="number"
-                    placeholder="Senf"
-                    name="mustard"
-                    max={getSauceSum(sauces.mustard)}
-                    min={0}
-                    value={sauces.mustard}
-                    onChange={(event) => {
-                      setSauce((prev) => {
-                        return {
-                          ...prev,
-                          [event.target.name]: Number(event.target.value),
-                        };
-                      });
-                    }}
-                  />
-                </Col>
-                <Col>
-                  <Form.Label>Süßer Senf</Form.Label>
-                  <FormControl
-                    type="number"
-                    placeholder="Süßer Senf"
-                    name="sweetMustard"
-                    max={getSauceSum(sauces.sweetMustard)}
-                    min={0}
-                    value={sauces.sweetMustard}
-                    onChange={(event) => {
-                      setSauce((prev) => {
-                        return {
-                          ...prev,
-                          [event.target.name]: Number(event.target.value),
-                        };
-                      });
-                    }}
-                  />
-                </Col>
-                <Form.Text className="text-muted">
-                  Die Anzahl der Saucen darf nicht größer als die Anzahl der
-                  Brötchen + 1 sein.
-                </Form.Text>
-              </Row>
-              <Row className="mt-3">
-                <Col>
-                  <Form.Label>Anzahl</Form.Label>
-                  <FormControl
-                    type="number"
-                    name="quantity"
-                    required
-                    max={10}
-                    placeholder="Anzahl der Brötchen"
-                    value={productCount}
-                    onChange={(event) => {
-                      setProductCount(Number(event.target.value));
-                    }}
-                  />
-                </Col>
-                <Col>
-                  <Form.Label>Kosten bei {productCount} Brötchen</Form.Label>
-                  <FormControl
-                    type="text"
-                    name="quantity"
-                    value={calcPrice()}
-                    max={10}
-                    placeholder="Kosten"
-                    disabled
-                  />
-                </Col>
-              </Row>
-              <Form.Text className="text-muted">
-                Die Anzahl der Brötchen ist auf 10 begrenzt. Falls bereits etwas
-                auf den selben Artikel bestellt wurde, wird das Brötchen mit
-                dieser Bestellung aktualisiert.
-              </Form.Text>
+          {isBreadSite ? (<>
+            <Row>
+              <Col>
+                <Form.Label>Brottyp</Form.Label>
+                <Form.Select name="bread">
+                  <option value="normal">Normal</option>
+                  <option value="multigrain">Mehrkorn</option>
+                </Form.Select>
+              </Col>
+            </Row>
+            <Row className="mt-3 mb-3">
+              <Col>
+                <Form.Label>Ketchup</Form.Label>
+                <FormControl
+                  type="number"
+                  placeholder="Ketchup"
+                  name="ketchup"
+                  max={getSauceSum(sauces.ketchup)}
+                  min={0}
+                  value={sauces.ketchup}
+                  onChange={(event) => {
+                    setSauce((prev) => {
+                      return {
+                        ...prev,
+                        [event.target.name]: Number(event.target.value),
+                      };
+                    });
+                  }}
+                />
+              </Col>
+              <Col>
+                <Form.Label>Senf</Form.Label>
+                <FormControl
+                  type="number"
+                  placeholder="Senf"
+                  name="mustard"
+                  max={getSauceSum(sauces.mustard)}
+                  min={0}
+                  value={sauces.mustard}
+                  onChange={(event) => {
+                    setSauce((prev) => {
+                      return {
+                        ...prev,
+                        [event.target.name]: Number(event.target.value),
+                      };
+                    });
+                  }}
+                />
+              </Col>
+              <Col>
+                <Form.Label>Süßer Senf</Form.Label>
+                <FormControl
+                  type="number"
+                  placeholder="Süßer Senf"
+                  name="sweetMustard"
+                  max={getSauceSum(sauces.sweetMustard)}
+                  min={0}
+                  value={sauces.sweetMustard}
+                  onChange={(event) => {
+                    setSauce((prev) => {
+                      return {
+                        ...prev,
+                        [event.target.name]: Number(event.target.value),
+                      };
+                    });
+                  }}
+                />
+              </Col>
+              {/* <Form.Text className="text-muted">
+              Hier kannst du die Anzahl der Saucen auswählen
+            </Form.Text> */}
+            </Row>
+          </>) : null}
+          <Row>
+            <Col>
+              <Form.Label>Anzahl</Form.Label>
+              <FormControl
+                type="number"
+                name="quantity"
+                required
+                max={10}
+                placeholder="Anzahl"
+                value={productCount}
+                onChange={(event) => {
+                  setProductCount(Number(event.target.value));
+                }}
+              />
+            </Col>
+            <Col>
+              <Form.Label>Preis für {productCount} Stück</Form.Label>
+              <FormControl
+                type="text"
+                name="quantity"
+                value={calcPrice()}
+                max={10}
+                placeholder="Kosten"
+                disabled
+              />
+            </Col>
+          </Row>
+          {/* <Form.Text className="text-muted">
+            Die Anzahl ist auf 10 pro Anfrage begrenzt
+          </Form.Text> */}
 
-              {menuAdditions.menuAdditions.length > 0 ? (
-                <Row className="mt-3">
-                  <Col>
-                    <Form.Label>Bemerkung (optional)</Form.Label>
-                    {menuAdditions.menuAdditions.map((option, index) => {
-                      if (option)
-                        return (
-                          <Form.Check
-                            type={'checkbox'}
-                            key={`comment-${index}`}
-                            name="comment"
-                            label={`${option.name} (+${formatToCurrent(
-                              option.price
-                            )} pro Brötchen)`}
-                            checked={comments.includes(option.name)}
-                            value={option.name}
-                            onChange={(event) => {
-                              if (event.target.checked) {
-                                setComments((prev) => {
-                                  return [...prev, event.target.value];
-                                });
-                              } else {
-                                setComments((prev) => {
-                                  return prev.filter(
-                                    (comment) => comment !== event.target.value
-                                  );
-                                });
-                              }
-                            }}
-                          />
-                        );
-                    })}
-                  </Col>
-                </Row>
-              ) : null}
-            </>
-          ) : (
-            <>
-              <Row>
-                <Col>
-                  <Form.Label>Anzahl</Form.Label>
-                  <FormControl
-                    type="number"
-                    name="quantity"
-                    required
-                    max={10}
-                    placeholder="Anzahl"
-                  />
-                </Col>
-                <Col>
-                  <Form.Label>
-                    Kosten bei {productCount} {popUpData.name}
-                  </Form.Label>
-                  <FormControl
-                    type="text"
-                    name="quantity"
-                    value={calcPrice()}
-                    max={10}
-                    placeholder="Kosten"
-                    disabled
-                  />
-                </Col>
-              </Row>
-              {menuAdditions.menuAdditions.length > 0 ? (
-                <Row className="mt-3">
-                  <Col>
-                    <Form.Label>Bemerkung (optional)</Form.Label>
-                    {menuAdditions.menuAdditions.map((option, index) => {
-                      if (option)
-                        return (
-                          <Form.Check
-                            type={'checkbox'}
-                            key={`comment-${index}`}
-                            name="comment"
-                            label={`${option.name} (+${formatToCurrent(
-                              option.price
-                            )} pro ${popUpData.name})`}
-                            value={option.name}
-                          />
-                        );
-                    })}
-                  </Col>
-                </Row>
-              ) : null}
-            </>
-          )}
+          {menuAdditions.menuAdditions.length > 0 ? (
+            <Row className="mt-3">
+              <Form.Label>Bemerkung (optional)</Form.Label>
+              {menuAdditions.menuAdditions.map((option, index) => {
+
+                if (option)
+                  return (
+                    <>
+                      {index % 2 === 0 ? <Row /> : null}
+                      <Col
+                        key={`comment-${index}`}
+                      >
+                        <Form.Check
+                          type={'checkbox'}
+                          name="comment"
+                          label={`${option.name} (+${formatToCurrent(
+                            option.price
+                          )} pro Brötchen)`}
+                          checked={comments.includes(option.name)}
+                          value={option.name}
+                          onChange={(event) => {
+                            if (event.target.checked) {
+                              setComments((prev) => {
+                                return [...prev, event.target.value];
+                              });
+                            } else {
+                              setComments((prev) => {
+                                return prev.filter(
+                                  (comment) => comment !== event.target.value
+                                );
+                              });
+                            }
+                          }}
+                        />
+                      </Col>
+                    </>
+                  );
+              })}
+            </Row>
+          ) : null}
         </Modal.Body>
         <Modal.Footer>
           <Button type="submit" variant="primary">
